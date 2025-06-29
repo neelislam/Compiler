bd80b6a9766493d2364da6f35946540eb7438761
rabiu <rabiul17.ri@gmail.com>
2025-06-29 10:41:06 +0600

    5+s

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
-#include <cctype>
 using namespace std;
 
 int main() {
-    string input = "5+s";
+    const string input = "5+s";
 
-    for(char ch : input){
-         if(ch >= '0' && ch <= '9'){
+    for(const char ch : input){
+        if(ch >= '0' && ch <= '9'){
            cout << ch << ": digit" << endl;
          }
         else if(ch == '+' || ch == '-' || ch == '*' || ch == '/') {
@@ -16,7 +15,7 @@ int main() {
         else if(ch >= 64 && ch <= 90 || ch >= 97 && ch <= 122) {
             cout << ch << ": Digit" << endl;
         }
-        else{
+        else {
             cout << ch << ": unknown symbol" << endl;
         }
     }
